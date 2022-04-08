ea8f05adbaa3fdb9f9f02c6629b54ebbd206c13d
HongSumin <googijnh4545@gmail.com>
2022-04-09 00:31:34 +0900

    nodemailer를 통해 메일 전송 셋팅

---
+++ src/sendEmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+const currentDate = require("./date");
+require("dotenv").config();
+
+async function sendMail(contents) {
+  let transporter = nodemailer.createTransport({
+    service: "naver",
+    host: "smtp.gmail.com",
+    secure: false,
+    auth: {
+      user: process.env.NAVER_ID, // 보내는 메일의 주소
+      pass: process.env.NAVER_PW, // 보내는 메일의 비밀번호
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: process.env.FROM_MAIL_ADRESS,
+    to: process.env.TO_MAIL_ADRESS,
+    subject: `[${currentDate.dateNow}]김포생활체육관 테니스장 예약 가능 코트`,
+    html: contents,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+}
+
+module.exports = sendMail;
